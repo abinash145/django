9fe2023a68ab88385214024f92a4cba8414c8148
unknown <abinashrai14@gmail.com>
2021-06-24 04:01:52 +0545

    view edit in index

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,15 @@
 
 from django.urls import path
-from .views import home
+from .views import *
 app_name="home"
 urlpatterns = [
     path('', home, name='home'),
+    path('about/', about,name='about'),
+    path('bloghome/',bloghome,name='bloghome'),
+    path('blogsingle/', blogsingle,name='blogsingle'),
+    path('contact/', contact,name='contact'),
+    path('elements/', elements,name='elements'),
+    path('portfolio/', portfolio,name ='portfolio'),
+    path('price/', price,name='price'),
+    path('services/', services,name='services'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
+	view={}
+	view["feedback"]=Feedback.objects.all()
+	return render(request,'index.html',view)
 
-	return render(request,'index.html')
+def about(request):
+	return render(request,'about.html')
+
+def bloghome(request):
+	return render(request,'blog-home.html')
+
+def blogsingle(request):
+	return render(request,'blog-single.html')
+
+def contact(request):
+	return render(request,'contact.html')
+
+def elements(request):
+	return render(request,'elements.html')
+
+def portfolio(request):
+	return render(request,'portfolio.html')
+
+def price(request):
+	return render(request,'price.html')		
+
+def services(request):
+	return render(request,'services.html')								
